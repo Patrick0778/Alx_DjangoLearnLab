ed9ff37372cce3df25b928a25ae2a798e8064872
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-02-19 13:38:15 +0300

    new work

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Book
 from .models import Library
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 def list_books(request):
     """
@@ -19,38 +20,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-    def register(request):
-    """
-    Handles user registration.
-    """
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("home")  # Redirect to homepage or dashboard
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
-
-def user_login(request):
-    """
-    Handles user login.
-    """
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("home")  # Redirect after successful login
-    else:
-        form = AuthenticationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
-
-def user_logout(request):
-    """
-    Handles user logout.
-    """
-    logout(request)
-    return render(request, "relationship_app/logout.html")
+    
