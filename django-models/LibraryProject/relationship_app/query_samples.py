9e673cfdfadf023975f0887c03838c71caa9bc8f
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-02-19 12:34:40 +0300

    Django is shit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
 # List all books in a library
 def get_books_in_library(library_name):
