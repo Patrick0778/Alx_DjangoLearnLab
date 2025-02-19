005dc0036ccbb06762efd366c7db31f757bf1ae9
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-02-19 12:54:26 +0300

    new work

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,5 +13,5 @@ class LibraryDetailView(DetailView):
     Class-based view to display details of a specific library.
     """
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
