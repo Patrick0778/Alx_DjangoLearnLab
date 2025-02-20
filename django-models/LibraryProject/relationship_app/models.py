5eba24d469cfacd15aacb2c3627e19d9d4c7fe30
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-02-20 08:38:56 +0300

    django project 3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# User Profile Model (Extends Django User)
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -15,12 +17,24 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
+# Automatically create a UserProfile when a new User is registered
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+# Book Model (Linked to Authors)
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -28,6 +42,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+# Library Model (Many Books can belong to a Library)
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book, related_name='libraries')
@@ -35,9 +50,11 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+# Librarian Model (Now Linked to Django's User)
 class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='librarian_profile')
     name = models.CharField(max_length=255)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
-        return self.name
+        return f"{self.name} (Librarian at {self.library.name})"
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+]
 
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
