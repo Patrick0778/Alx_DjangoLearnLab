645192641eacc7ec683eb535446f8893ace79f73
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-03-19 16:06:51 +0300

    updates views.py and serializers.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from api.views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView  # Import your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
