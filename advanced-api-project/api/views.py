06fd14c5bdcdf5222bc2ad28963b4aa00dceb8b1
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-03-19 16:39:21 +0300

    updates views.py and serializers.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework as django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
