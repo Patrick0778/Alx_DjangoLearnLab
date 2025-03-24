c4e81a57e920196dba1e3c26f20d1d2c48f58ac7
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-03-24 12:41:03 +0300

    edited serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,35 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
-class UserSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)  # Create authentication token
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid username or password.")
+        token, created = Token.objects.get_or_create(user=user)
+        return {'user': user, 'token': token.key}
+# Compare this snippet from social_media_api/social_media_api/urls.py:
